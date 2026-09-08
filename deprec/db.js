ccf797c4fba40b2b2657923d83cc056802eb53f6
oscarZ1 <118237839+oscarZ1@users.noreply.github.com>
2026-09-07 21:39:55 -0700

    Deleted dead files

--- deprec/db.js
+++
@@ -1,9 +0,0 @@
-import {Pool} from "pg";
-
-export const pool = new Pool({
-  user: "postgres", 
-  password: process.env.DB_PASSWORD, 
-  host: "localhost", 
-  database: "AhnstoppableLearning", 
-  port: 5432
-});
