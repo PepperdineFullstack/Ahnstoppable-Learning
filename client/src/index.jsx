1581351b2a0280ded62d3b7e0b95f717e30bffe1
oscarZ1 <118237839+oscarZ1@users.noreply.github.com>
2026-09-10 18:14:04 -0700

    Seperated frontend and backend

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,10 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import "./styles.css";
 
 //connects to the index.html file
 const root = createRoot(document.getElementById('root'));
-
 
 //renders app in html file: inserts <App /> code
 root.render(
